bca54e06bbc8d083bc6630fe36178c1a274c98e1
Oryto Mikli <orytomikli21@gmail.com>
2023-04-30 13:41:20 +0700

    setup tailwind and model migration and controllers

---
+++ app/Http/Controllers/PemilikUsahaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PemilikUsaha;
+use Illuminate\Http\Request;
+
+class PemilikUsahaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PemilikUsaha $pemilikUsaha)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PemilikUsaha $pemilikUsaha)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PemilikUsaha $pemilikUsaha)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PemilikUsaha $pemilikUsaha)
+    {
+        //
+    }
+}
---
+++ app/Models/Pendana.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pendana extends Model
+{
+    use HasFactory;
+}
