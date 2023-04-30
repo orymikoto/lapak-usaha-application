d7239ca0b09bce0f838668dbce6255b776209f4b
orymikoto <rosbergroll99@gmail.com>
2023-04-30 21:16:35 +0700

    navbar and layour welcome page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<div class="w-full py-2 flex bg-neutral-800 px-8">
+  <div class="flex flex-col items-center">
+    <img src="/images/logo.png" class="object-cover object-center w-12 h-12" alt="logo vestry">
+    <p class="text-white text-sm font-semibold">Vestry Application</p>
+  </div>
+  <div class="flex-1 flex items-center justify-around">
+
+  </div>
+  <div class="flex gap-x-2 items-center w-[14rem]">
+    <a href="/login"
+      class="text-center text-white text-lg font-medium hover:bg-white rounded-full py-1 bg-yellow-500 hover:text-yellow-500 hover:shadow-md hover:shadow-rose-500/50 cursor-pointer flex-1">
+      Login</a>
+    <a href="/register"
+      class="text-center text-white text-lg font-medium hover:bg-white rounded-full py-1 bg-rose-700 hover:text-rose-700 hover:shadow-md hover:shadow-yellow-500/50 cursor-pointer flex-1">
+      Register</a>
+  </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   @vite('resources/js/app.js')
 
-  <title>Laravel</title>
+  <title>Vestry</title>
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,8 +15,38 @@
 
 </head>
 
-<body class="antialiased bg-white min-h-0 w-full flex flex-col">
+<body class="antialiased bg-neutral-100 min-h-screen w-full flex flex-col">
+  <x-navbar />
 
+  <div class="flex-1 flex flex-col px-12 items-center ">
+    <h1 class="text-yellow-500 font-semibold text-5xl my-4  text-center">Vestry Application</h1>
+    <div class=" grid grid-cols-12 gap-4 bg-white mt-20 rounded-md p-4 md:w-[42rem] sm:w-[32rem] w-[20rem] shadow-md shadow-black/50">
+      <div class="md:col-span-3 sm:col-span-6 col-span-full flex flex-col gap-y-2 items-center justify-start h-full text-center">
+        <img src="" class="w-10 h-10 object-contain object-center" alt="">
+        <h3 class="text-amber-400 font-medium">Judul Fitur</h3>
+        <p class="text-neutral-400 text-sm font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, quia?</p>
+      </div>
+      <div class="md:col-span-3 sm:col-span-6 col-span-full flex flex-col gap-y-2 items-center justify-start h-full text-center">
+        <img src="" class="w-10 h-10 object-contain object-center" alt="">
+        <h3 class="text-amber-400 font-medium">Judul Fitur</h3>
+        <p class="text-neutral-400 text-sm font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, quia?</p>
+      </div>
+      <div class="md:col-span-3 sm:col-span-6 col-span-full flex flex-col gap-y-2 items-center justify-start h-full text-center">
+        <img src="" class="w-10 h-10 object-contain object-center" alt="">
+        <h3 class="text-amber-400 font-medium">Judul Fitur</h3>
+        <p class="text-neutral-400 text-sm font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, quia?</p>
+      </div>
+      <div class="md:col-span-3 sm:col-span-6 col-span-full flex flex-col gap-y-2 items-center justify-start h-full text-center">
+        <img src="" class="w-10 h-10 object-contain object-center" alt="">
+        <h3 class="text-amber-400 font-medium">Judul Fitur</h3>
+        <p class="text-neutral-400 text-sm font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, quia?</p>
+      </div>
+    </div>
+    <div
+      class="w-[21rem] cursor-pointer my-4 font-bold text-center py-2 bg-yellow-500 text-xl text-white hover:shadow-md hover:shadow-black/40 hover:text-yellow-500 hover:bg-white rounded-full duration-200">
+      START USING VESTRY
+    </div>
+  </div>
 </body>
 
 </html>
