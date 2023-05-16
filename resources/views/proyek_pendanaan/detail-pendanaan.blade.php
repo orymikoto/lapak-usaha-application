183d8ed1471546e4fa1d88837cdd054f7b3dead3
orymikoto <rosbergroll99@gmail.com>
2023-05-16 14:20:20 +0700

    update modal show

--- resources/views/proyek_pendanaan/detail-pendanaan.blade.php
+++ resources/views/proyek_pendanaan/detail-pendanaan.blade.php
@@ -7,11 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/js/app.js')
 
-  <title>Document</title>
+  <title>Vestry</title>
 </head>
 
 <body class="antialiased bg-neutral-200 min-h-screen w-full flex flex-col overflow-x-hidden ">
   <x-navbar />
+  
+  <x-modal-show-picture judul="Foto Deskripsi Usaha" urlFoto="{{$detailPendanaan->deskripsiUsaha->foto_usaha}}" />
   <div class="flex-1 flex flex-col w-full items-center gap-4">
     <h1 class="font-righteous text-3xl text-yellow-500 my-2">Detail Pendanaan</h1>
     <div class="bg-white rounded-lg p-4 flex flex-col items-center w-[40rem] shadow-[2px_3px_7px_1px_rgba(0,0,0,0.3)]">
@@ -58,6 +60,57 @@
           <div class="p-2 rounded-md w-full bg-neutral-200">
             {{ $detailPendanaan->deskripsiUsaha->deskripsi }}
           </div>
+        </div>
+      </div>
+
+      {{-- FILE KONTRAK --}}
+      <h2 class="text-neutral-700 font-medium font-righteous text-xl text-center mt-4">File Kontrak</h2>
+      <div class="flex w-[70%] gap-4">
+        <div class="flex flex-col cursor-pointer items-center text-neutral-600 font-medium font-roboto hover:text-yellow-500 duration-200 flex-1 hover:bg-neutral-200">
+          <img src="/icons/pdf.svg" class="w-16 h-16 " alt="">
+          <p>Admin</p>
+        </div>
+        <div class="flex flex-col cursor-pointer items-center text-neutral-600 font-medium font-roboto hover:text-yellow-500 duration-200 flex-1 hover:bg-neutral-200">
+          <img src="/icons/pdf.svg" class="w-16 h-16 " alt="">
+          <p>Pendana</p>
+        </div>
+        <div class="flex flex-col cursor-pointer items-center text-neutral-600 font-medium font-roboto hover:text-yellow-500 duration-200 flex-1 hover:bg-neutral-200">
+          <img src="/icons/pdf.svg" class="w-16 h-16 " alt="">
+          <p>Pengusaha</p>
+        </div>
+      </div>
+
+      {{-- FILE FOTO --}}
+      <h2 class="text-neutral-700 font-medium font-righteous text-xl text-center mt-4">File Foto</h2>
+      <div class="flex w-[70%] gap-4 ">
+        <div class="flex flex-col cursor-pointer items-center text-neutral-600 font-medium font-roboto hover:text-yellow-500 duration-200 flex-1 hover:bg-neutral-200">
+          <img src="/icons/img.svg" class="w-16 h-16 " alt="">
+          <p>Deskripsi Usaha</p>
+        </div>
+        <div class="flex flex-col cursor-pointer items-center text-neutral-600 font-medium font-roboto hover:text-yellow-500 duration-200 flex-1 hover:bg-neutral-200">
+          <img src="/icons/img.svg" class="w-16 h-16 " alt="">
+          <p>Bukti Pembayaran</p>
+        </div>
+        <div class="flex flex-col cursor-pointer items-center text-neutral-600 font-medium font-roboto hover:text-yellow-500 duration-200 flex-1 hover:bg-neutral-200">
+          <img src="/icons/img.svg" class="w-16 h-16 " alt="">
+          <p>Bukti Bagi Hasil</p>
+        </div>
+      </div>
+
+      {{-- ACTIONS --}}
+      <h2 class="text-neutral-700 font-medium font-righteous text-xl text-center mt-4">Tambah Data</h2>
+      <div class="flex gap-4 w-[70%]">
+        <div class="hover:text-yellow-500 py-1 flex flex-col items-center flex-1 hover:bg-neutral-200 duration-200 cursor-pointer ">
+         <img src="/icons/upload.svg" class="w-10 h-10 " alt="">
+          <p>File Kontrak</p>
+        </div>
+        <div class="hover:text-yellow-500 py-1 flex flex-col items-center flex-1 hover:bg-neutral-200 duration-200 cursor-pointer ">
+         <img src="/icons/upload.svg" class="w-10 h-10 " alt="">
+          <p>Bukti Pembayaran</p>
+        </div>
+        <div class="hover:text-yellow-500 py-1 flex flex-col items-center flex-1 hover:bg-neutral-200 duration-200 cursor-pointer ">
+         <img src="/icons/upload.svg" class="w-10 h-10 " alt="">
+          <p>Bukti Bagi Hasil</p>
         </div>
       </div>
     </div>
